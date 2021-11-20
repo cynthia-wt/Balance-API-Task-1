fde798b39431e5f6d9590463a4680b5e5a2968de
cynthiaow <cynthia.ow-ext@block.one>
2021-11-20 17:25:02 +0800

    added node_modules

---
+++ src/main/index.js
@@ -0,0 +1,108 @@
+import express from 'express';
+import fetch from 'node-fetch';
+const app = express();
+export { app };
+
+const assets = {
+  BTC: "btc",
+  ETH: "eth"
+}
+
+const userBalances = {
+  "100001": {
+    "BTC": "0.5",
+    "ETH": "2"
+  },
+  "100002": {
+    "BTC": "0.1"
+  },
+  "100003": {
+    "ETH": "0.7"
+  },
+  "100004": {
+    "BTC": "12",
+    "ETH": "3"
+  },
+  "100005": {
+    "BTC": "4",
+    "ETH": "3"
+  }
+};
+
+async function fetchTickerData(baseAsset) {
+  const url = `http://www.bitstamp.net/api/v2/ticker/${baseAsset}usd`;
+  const options = {
+    "method": "GET",
+    "headers": {
+      "Content-Type": "application/json"
+    },
+  }
+
+  const tickerData = await fetch(url, options)
+    .then(res => res.json())
+    .catch((error) => {
+      console.error("Failed to fetch ticker data from Bitstamp");
+      throw new error("Failed to fetch ticker data from Bitstamp");
+    });
+
+  return tickerData;
+}
+
+function roundToTwo(num) {
+  return + (Math.round(num + "e+2") + "e-2");
+}
+
+export async function calculateTotal(assetBalances, btcPrice, ethPrice) {
+  let totalBalance = 0;
+  let lastPrice;
+
+  for (const asset in assetBalances) {
+    if (Object.values(assets).includes(asset.toLowerCase())) {
+
+      if (asset == "BTC") {
+        lastPrice = btcPrice;
+      } else {
+        lastPrice = ethPrice;
+      }
+
+      totalBalance += lastPrice * assetBalances[asset];
+
+    } else {
+
+      // Example of bad asset data in database: "XYZ": "1", where asset XYZ does not exist
+      console.error(`Bad data : Asset ${asset} is invalid`);
+      throw new error(`Bad data : Asset ${asset} is invalid :`)
+    }
+  }
+
+  return totalBalance;
+}
+
+app.get('/users/:id/total-balance', async (req, res, next) => {
+  const userId = req.params.id;
+
+  try {
+    if (!userBalances[userId]) {
+      console.error(`Invalid input : User id ${userId} is not found`);
+      res.status(404).send({ "message": `Invalid input - User id ${userId} is not found` });
+
+    } else {
+      const btcPrice = (await fetchTickerData("btc")).last;
+      const ethPrice = (await fetchTickerData("eth")).last;
+      const totalBalance = await (calculateTotal(userBalances[userId], btcPrice, ethPrice));
+      res.status(200).send({ [userId]: `${roundToTwo(totalBalance)}USD` });
+    }
+  } catch (error) {
+    console.error(`Failed to retrieve total balance for user ${userId}`);
+    res.status(500).send({ "message": `Failed to retrieve total balance for user ${userId}` })
+  }
+}
+)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(8080, () => {
+  console.log(`Example app listening at http://localhost:8080`)
+})
---
+++ src/test/test.js
@@ -0,0 +1,76 @@
+import chai from 'chai';
+import { app, calculateTotal } from './index.js';
+import httpChai from 'chai-http';
+
+const { assert } = chai;
+chai.use(httpChai);
+
+const testUserBalances = {
+  "100001": {
+    "BTC": "0.5",
+    "ETH": "2"
+  },
+  "100002": {
+    "BTC": "0.1"
+  },
+  "100003": {
+    "ETH": "0.7"
+  },
+  "100004": {
+    "BTC": "12",
+    "ETH": "3"
+  },
+  "100005": {
+    "BTC": "4",
+    "ETH": "3"
+  }
+};
+
+// E2ETest - Check if we are able to retrieve the total balance of the user properly
+describe('/GET user total balance', () => {
+  it('it should GET total balance of user in USD', (done) => {
+
+    for (const userBalance in testUserBalances) {
+      chai.request(app)
+        .get(`/users/${userBalance}/total-balance`)
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.notEqual(res.body, null);
+          assert.equal(typeof (res.body), 'object');
+        }); 
+    }
+
+    done(); 
+  });
+})
+
+// E2ETest - Check if we receive an error message if the user id is invalid 
+describe('/GET user total balance', () => {
+  it('it should return error message', (done) => {
+      chai.request(app)
+        .get(`/users/10200/total-balance`)
+        .end((err, res) => {
+          assert.equal(res.status, 404);
+          assert.notEqual(res.body, null);
+          assert.equal(res.body["message"], `Invalid input - User id 10200 is not found`);
+        }); 
+        done();  
+    });
+})
+
+// UnitTest - Check that the calculation of the total balance is correct when user has balance under both markets i.e. both ETHUSD and BTCUSD
+describe('UserBalances', async () => {
+  it('Assuming BTC is 60000 and ETH is 3000, user has 0.5 BTC and 2 ETH return 36000', async () => {
+    let result = await calculateTotal(testUserBalances["100001"], 60000, 3000);
+    assert.equal(result, 36000);
+  });
+});
+
+// UnitTest - Check that the calculation of the total balance is correct when user has balance under only a single market i.e. only ETHUSD
+describe('UserBalances', async () => {
+  it('Assuming BTC is 60000, user has 0.5 BTC and 2 ETH return 36000', async () => {
+    let result = await calculateTotal(testUserBalances["100002"], 60000, 3000);
+    assert.equal(result, 6000);
+  });
+});
+
